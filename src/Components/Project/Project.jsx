01ce71a7141a2311f7d18c25f1df9f09882c645f
Jordan Chung <jordanchungy@gmail.com>
2022-07-15 23:54:39 -0500

    Live Demo Button fixed

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -45,7 +45,7 @@ const Project = () => {
                 <h3>{title}</h3>
                 <div className="portfolio__item-cta">
                   <a href={github} className='btn' target='_blank'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
                 </div>
               </article>
             )
